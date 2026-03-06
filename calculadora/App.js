1996e149cbcb668f7cf8ae1552198c88502be60d
pedrodolopes22-max <pedrodolopes22@gmail.com>
2026-03-06 11:36:27 -0300

    AULA SEIS

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+const [display, setDisplay] = useState('0');
+const [previousValue, setPreviousValue] = useState(null);
+const [operator, setOperator] = useState(null);
+const [waitingForNewValue, setWaitingForNewValue] = useState(false);
+
+const handleNumberPress = (num) => {
+  if (waitingForNewValue){
+    setDisplay(String(num));
+    setWaitingForNewValue(false);
+  }else {
+    setDisplay(display ==='0' ? String(num) : display + num)
+  }
+}
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
 
+           {/* Display da Calculadora */}
         <View style={styles.displayContainer}>
           <Text style={styles.displayText} numberOfLines={1}>
-            1634,00
+            {display}
           </Text>
      </View>
 
@@ -71,6 +87,32 @@ export default function App() {
             <Text style={styles.operatorButton}>+</Text>
           </TouchableOpacity>
         </View>
+        
+        <View style={styles.row}>
+          <TouchableOpacity style={[styles.buttonNumber, styles.doubleWidthButton]}>
+            <Text style={styles.buttonText}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonNumber}>
+            <Text style={styles.buttonText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.operatorButton}>
+            <Text style={styles.operatorText}>=</Text>
+          </TouchableOpacity>
+        
+        </View>
+        
+        <View style={styles.row}>
+          <TouchableOpacity style={[styles.buttonNumber, styles.doubleWidthButton]}>
+            <Text style={styles.buttonText}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonNumber}>
+            <Text style={styles.buttonText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.operatorButton}>
+            <Text style={styles.operatorText}>=</Text>
+          </TouchableOpacity>
+        </View>
+
 
         <View style={styles.row}>
           <TouchableOpacity style={[styles.buttonNumber, styles.doubleWidthButton]}>
@@ -83,7 +125,7 @@ export default function App() {
             <Text style={styles.operatorText}>=</Text>
           </TouchableOpacity>
         </View>
-        
+
     </View>
   );
 }
